839e6faae729b842205d905469e42bc3120fc011
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 11:18:54 +0600

    first commit

---
+++ if_else_if_else.php
@@ -0,0 +1,50 @@
+<?php 
+$a = 6;
+$b = 7;
+
+if($a>$b){
+	echo 'a is greater than b';
+}
+else{
+	echo 'b is greater than a';
+}
+//####################
+if($a>$b){
+	echo 'a is greater than b';
+}
+else if($a<$b){
+	echo 'b is greater than a';
+}
+//#####################
+
+if($a>$b){
+	echo 'a is greater than b';
+}
+else if($a<$b){
+	echo 'b is greater than a';
+}
+else if($a==$b){
+	echo 'b is equal to a';
+}
+//######################
+$a = 8;
+$b = 7;
+if($a>$b){
+	echo 'a is greater than b';
+}
+if($a==8){
+	echo ' a = 8';
+}
+//######################
+
+$a = 8;
+$b = 7;
+if($a>$b){
+	echo 'a is greater than b';
+}
+else if($a==8){
+	echo ' a = 8';
+}
+
+
+?>
